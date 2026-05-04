836e0837b9cc9a1452fd3e1a37390c8799d97c1c
nhan-n9 <nhannguyen.learn@gmail.com>
2026-05-04 22:01:17 +0700

    setup fe + ci

---
+++ FE/src/api/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    timeout: 10000 // 10 seconds
+});
+
+export default api;
---
+++ FE/src/setupTests.js
@@ -0,0 +1,3 @@
+// react-testing-library renders components to document.body,
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom';
