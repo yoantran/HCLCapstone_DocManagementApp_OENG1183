a70aaa9f83b5968bdd7bd084b62746f2dc5c8289
yoantran <s3927023@rmit.edu.vn>
2026-06-30 17:02:29 +0700

    [54] implemented switch between user & department

--- FE/src/pages/AdminManagement.jsx
+++ FE/src/pages/AdminManagement.jsx
@@ -1,6 +1,34 @@
+import {useState} from "react";
+import {CustomButton} from "../components/button/index.jsx";
+
 export default function AdminManagement() {
+    const [activeTab, setActiveTab] = useState('users');
+
     return (
         <>
+            <div>
+                <h2>
+                    {activeTab === 'users' ? 'User Management' : 'Department Management'}
+                </h2>
+                <div className={"inline-flex"}>
+                    <CustomButton
+                        onClick={() => setActiveTab('users')}
+                        pill
+                        color={activeTab === 'users' ? 'primary' : 'alternative'}
+                        className={"transition-colors"}
+                    >
+                        User Management
+                    </CustomButton>
+                    <CustomButton
+                        onClick={() => setActiveTab('departments')}
+                        pill
+                        color={activeTab === 'departments' ? 'primary' : 'alternative'}
+                        className={"transition-colors"}
+                    >
+                        Department Management
+                    </CustomButton>
+                </div>
+            </div>
         </>
     )
 }
