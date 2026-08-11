4a8caeeca41ab8f20e8b93f17fe9e3a3ac435e75
yoantran <s3927023@rmit.edu.vn>
2026-08-11 20:01:44 +0700

    [109] made footer sticky and MainLayout not overflow

--- FE/src/layouts/MainLayout.jsx
+++ FE/src/layouts/MainLayout.jsx
@@ -28,7 +28,7 @@ export default function MainLayout() {
 
     return (
         <div
-            className="border-r border-(--cool-gray-200)"
+            className="border-r border-(--cool-gray-200) h-screen w-screen overflow-hidden flex flex-col"
         >
             {/* Global Navbar*/}
             <Navbar
@@ -111,7 +111,7 @@ export default function MainLayout() {
             <main className="flex-1 p-8">
                 <Outlet />
             </main>
-            <div className="p-8 pt-0">
+            <div className="p-8 pt-0 sticky bottom-0 z-10">
                 <Footer className="bg-transparent border-0 rounded-none px-0 " />
             </div>
         </div>
